46bf18f42aad6da7ec916b01d375d360e0aef3c6
Noran Diaconu <norandiaconu@gmail.com>
2026-05-12 21:47:56 -0400

    Save output to file

--- run.py
+++ run.py
@@ -6,10 +6,12 @@ spinner.start()
 
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+import time
 
 index = 0
 nextId = ''
 array = []
+htmlArray = []
 
 def main():
     try:
@@ -27,6 +29,7 @@ def main():
 
 def run(client_id, client_secret, skips):
     spinner.text = "Loading..."
+    time.sleep(1)
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
         client_id=client_id,
         client_secret=client_secret,
@@ -34,12 +37,14 @@ def run(client_id, client_secret, skips):
         scope='user-library-read, user-follow-read'))
     currentLimit = 0
 
+    time.sleep(1)
     artists = sp.current_user_followed_artists(limit=50)['artists']
     total = artists['total']
     getAlbums(total, skips, sp, artists)
 
     while currentLimit < total:
         currentLimit += 50
+        time.sleep(1)
         artists = sp.current_user_followed_artists(limit=50, after=nextId)['artists']
         getAlbums(total, skips, sp, artists)
     sortAlbums()
@@ -52,6 +57,7 @@ def getAlbums(total, skips, sp, artists):
         spinner.text = f'{index / total * 100:.0f}% ({index}/{total}) | {artistName}'
         nextId = artist['id']
         if (artistName not in skips):
+            time.sleep(0.5)
             albums = sp.artist_albums(artist['uri'], album_type='album')
             for album in albums['items']:
                 array.append(
@@ -59,13 +65,20 @@ def getAlbums(total, skips, sp, artists):
                     ' \x1b[0;32;40m' + artistName.ljust(28) +
                     ' \x1b[0;33;40m' + album['name'] + '\x1b[0m'
                 )
+                htmlArray.append('<span class="r">' + album['release_date'] + '</span>&nbsp;<span class="g">' + artistName + '</span>&nbsp;<span class="y">' + album['name'] + '</span>')
 
 def sortAlbums():
+    array.sort()
     spinner.stop()
     print()
-    array.sort()
     for item in array:
         print(item)
+    htmlArray.sort()
+    f = open('index.html', 'w')
+    f.write('<html>\n<link rel="stylesheet" href="styles.css">')
+    for htmlItem in htmlArray:
+        f.write('\n<div>' + htmlItem + '</div>')
+    f.write('</html>')
 
 if __name__ == "__main__":
     main()
